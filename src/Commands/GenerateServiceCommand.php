e9349b4c65feab4571d48577757c97495de0efba
iqbalatma <iqbalatma@gmail.com>
2023-07-30 13:56:45 +0700

    refactoring generate service command console

--- src/Commands/GenerateServiceCommand.php
+++ src/Commands/GenerateServiceCommand.php
@@ -13,6 +13,11 @@ class GenerateServiceCommand extends Command
     protected $description = "Generate new service";
 
     protected Filesystem $files;
+    protected const STUB_PATH = __DIR__ . '/../Stubs/Service.stub';
+    protected string $targetPath;
+    protected string $singularClassName;
+
+
 
     /**
      * @param Filesystem $files
@@ -27,113 +32,92 @@ class GenerateServiceCommand extends Command
 
     public function handle()
     {
-        $path = $this->getSourceFilePath();
+        $this->setSingularClassName()
+            ->setTargetFilePath()
+            ->makeDirectory();
 
-        $this->makeDirectory(dirname($path));
 
-        $contents = $this->getSourceFile();
-
-        if (!$this->files->exists($path)) {
-            $this->files->put($path, $contents);
-            $this->info("File : {$path} created");
+        if (!$this->files->exists($this->targetPath)) {
+            $this->files->put($this->targetPath, $this->getTemplateFileContent());
+            $this->info("File : {$this->targetPath} created");
         } else {
-            $this->info("File : {$path} already exits");
+            $this->info("File : {$this->targetPath} already exits");
         }
     }
 
-    /**
-     * @return string
-     */
-    public function getStubPath():string
-    {
-        return __DIR__ . '/../Stubs/Service.stub';
-    }
 
 
     /**
      * @return array
      */
-    public function getStubVariables():array
+    private function getStubVariables(): array
     {
-        $name = $this->getSingularClassName($this->argument('name'));
+        $singularClassName = $this->singularClassName;
 
-        $explodedClassName = explode("/", $name);
-        $className = end($explodedClassName);
-
+        $explodedClassName = explode("/", $singularClassName);
 
         $namespace = "";
-        $explodedNamespace = explode("/", $name);
-        if(count($explodedNamespace) > 1){
+        $explodedNamespace = explode("/", $singularClassName);
+
+        // when namespace is more than 1 segment, remove last part because it is class name, and get previous part because its namespace dir
+        if (count($explodedNamespace) > 1) {
             array_pop($explodedNamespace);
-            $namespace = "\\".implode("\\", $explodedNamespace);
+            $namespace = "\\" . implode("\\", $explodedNamespace);
         }
 
         return [
-            'NAMESPACE'         => 'App\\Services'.$namespace,
-            'CLASS_NAME'        => $className
+            'NAMESPACE' => 'App\\Services' . $namespace,
+            'CLASS_NAME' => end($explodedClassName)
         ];
     }
 
-    /**
-     * Return the Singular Capitalize Name
-     * @param $name
-     * @return string
-     */
-    public function getSingularClassName($name):string
-    {
-        return ucwords(Pluralizer::singular($name));
-    }
 
     /**
-     * Get the stub path and the stub variables
-     *
-     * @return bool|mixed|string
-     *
-     */
-    public function getSourceFile()
-    {
-        return $this->getStubContents($this->getStubPath(), $this->getStubVariables());
-    }
-
-    /**
-     * @param $stub
-     * @param $stubVariables
      * @return array|false|string|string[]
      */
-    public function getStubContents($stub , $stubVariables = [])
+    private function getTemplateFileContent()
     {
-        $contents = file_get_contents($stub);
+        $content = file_get_contents(self::STUB_PATH);
 
-        foreach ($stubVariables as $search => $replace)
-        {
-            $contents = str_replace('$'.$search.'$' , $replace, $contents);
+        foreach ($this->getStubVariables() as $search => $replace) {
+            $content = str_replace("*$search*", $replace, $content);
         }
 
-        return $contents;
+        return $content;
     }
 
     /**
-     * Get the full path of generate class
-     *
-     * @return string
+     * @return self
      */
-    public function getSourceFilePath()
+    private function setSingularClassName(): self
     {
-        return base_path('app/Services') .'/' .$this->getSingularClassName($this->argument('name')) . '.php';
+        $this->singularClassName = ucwords(Pluralizer::singular($this->argument('name')));
+        return $this;
     }
 
+
+
     /**
-     * Build the directory for the class if necessary.
-     *
-     * @param  string  $path
-     * @return string
+     * @return self
      */
-    protected function makeDirectory($path)
+    private function setTargetFilePath(): self
     {
-        if (! $this->files->isDirectory($path)) {
-            $this->files->makeDirectory($path, 0777, true, true);
+        $className = $this->singularClassName;
+        $this->targetPath = base_path('app/Services') . "/$className.php";
+
+        return $this;
+    }
+
+
+    /**
+     * @return $this
+     */
+    private function makeDirectory(): self
+    {
+        if (!$this->files->isDirectory(dirname($this->targetPath))) {
+            $this->files->makeDirectory(dirname($this->targetPath), 0777, true, true);
         }
 
-        return $path;
+        return $this;
     }
 }
